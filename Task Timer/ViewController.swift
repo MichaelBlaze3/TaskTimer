8f1f16a39570b278094b43050167714f8fe84ac2
Michael Blas <michaelblas@michaels-macbook-pro.local>
2018-09-23 14:28:13 -0700

    Initial Commit

---
+++ Task Timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task Timer
+//
+//  Created by Michael Blas on 9/23/18.
+//  Copyright © 2018 Michael Blas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
